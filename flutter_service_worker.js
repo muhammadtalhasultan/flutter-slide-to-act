77b628db3f85147d88f8f7d263581858b73cfdea
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2020-05-04 17:56:04 +0200

    Built web
    Branch: master
    Commit: 7699449b8d007e430807ddc00a49965d44c1ee0a (dirty)
    
    package:peanut 3.5.0

---
+++ flutter_service_worker.js
@@ -0,0 +1,39 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "index.html": "926cd8008bef60a4e735ba8d18fbac9c",
+"/": "926cd8008bef60a4e735ba8d18fbac9c",
+"main.dart.js": "f65fab32220db4aa5ef37372bd5e4c77",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "00e0b69b49487ce4f9ff0c5fac8fda49",
+"assets/LICENSE": "1e52375f788d9afd20e8b8de7e4d8f11",
+"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
+"assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
